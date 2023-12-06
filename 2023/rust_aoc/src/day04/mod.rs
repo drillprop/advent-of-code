16b308c927be1d11825c49325adbb4ef160b30e7
drillprop <drylbartosz@gmail.com>
2023-12-06 13:16:41 +0100

    day 4 setup

---
+++ 2023/rust_aoc/src/day04/mod.rs
@@ -0,0 +1,13 @@
+// use crate::utils;
+
+pub fn a() -> u32 {
+    // let contents = utils::get_content("./src/day04/input.txt");
+
+    0
+}
+
+pub fn b() -> u32 {
+    // let contents = utils::get_content("./src/day04/input.txt");
+
+    0
+}
--- 2023/rust_aoc/src/main.rs
+++ 2023/rust_aoc/src/main.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 mod utils;
 
 fn main() {
@@ -16,4 +17,8 @@ fn main() {
     println!("day3 a: {result}");
     let result = day03::b();
     println!("day3 b: {result}");
+    let result = day04::a();
+    println!("day4 a: {result}");
+    let result = day04::b();
+    println!("day4 b: {result}");
 }
